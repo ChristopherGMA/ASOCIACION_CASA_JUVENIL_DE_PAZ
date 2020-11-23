9266b99b1a4a77026d7cb099576cd44caf596f4e
chris alvarado <chistofer1999@gmail.com>
2020-11-23 14:07:44 -0600

    Agregar archivos de proyecto.

---
+++ ASOCIACION_CASA_JUVENIL_DE_PAZ/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASOCIACION_CASA_JUVENIL_DE_PAZ.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
